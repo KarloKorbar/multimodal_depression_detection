cc4d4990410c48412f82aaec70245eff9c77dc88
karlo <karlo.korbi@gmail.com>
2025-03-04 19:00:05 +0100

    cleaned up data loading

---
+++ DepressionDetection/data_load/loader.py
@@ -0,0 +1,228 @@
+import os
+import pandas as pd
+from typing import List, Optional, Dict, Tuple, Any
+import data_load.pp_text as pp_text
+import data_load.pp_audio as pp_audio
+import data_load.pp_face as pp_face
+
+
+class DataLoader:
+    def __init__(self, base_directory: str = "data"):
+        self.base_directory = base_directory
+        self.phq_paths = [
+            "testing/dev_split_Depression_AVEC2017.csv",
+            "testing/full_test_split.csv",
+            "testing/train_split_Depression_AVEC2017.csv",
+        ]
+        self.feature_map = {}
+
+    def get_path_map(
+        self, required_files: List[str] = [], folder_ids: Optional[List[int]] = None
+    ) -> pd.DataFrame:
+        data = []
+        subfolders = [
+            f
+            for f in os.listdir(self.base_directory)
+            if os.path.isdir(os.path.join(self.base_directory, f))
+        ]
+
+        if folder_ids:
+            subfolders = [f for f in subfolders if int(f.split("_")[0]) in folder_ids]
+
+        for subfolder in subfolders:
+            subfolder_path = os.path.join(self.base_directory, subfolder)
+            formatted_subfolder = subfolder.split("_")[0]
+            subfolder_files = {"ID": formatted_subfolder}
+
+            for file_name in required_files:
+                file_path = os.path.join(subfolder_path, f"{subfolder[:3]}_{file_name}")
+                formatted_file_name = file_name.split(".")[0]
+                subfolder_files[formatted_file_name] = (
+                    file_path if os.path.exists(file_path) else None
+                )
+
+            data.append(subfolder_files)
+
+        df = pd.DataFrame(data)
+        df["ID"] = df["ID"].astype(int)
+        return df
+
+    def get_balanced_subset(self, percentage: float, random_state: int) -> List[int]:
+        df_all = self.get_path_map()
+        df_all = self._append_PHQ_Binary(df_all)
+
+        target_size = int(len(df_all) * percentage)
+        df_0 = df_all[df_all["PHQ_Binary"] == 0]
+        df_1 = df_all[df_all["PHQ_Binary"] == 1]
+
+        max_samples_per_class = min(len(df_0), len(df_1), target_size // 2)
+
+        sampled_0 = df_0.sample(n=max_samples_per_class, random_state=random_state)
+        sampled_1 = df_1.sample(n=max_samples_per_class, random_state=random_state)
+
+        balanced_subset = (
+            pd.concat([sampled_0, sampled_1])
+            .sample(frac=1, random_state=random_state)
+            .reset_index(drop=True)
+        )
+
+        return balanced_subset["ID"].to_list()
+
+    def _append_PHQ_Binary(self, df: pd.DataFrame) -> pd.DataFrame:
+        phq_dataframes = []
+
+        for path in self.phq_paths:
+            phq_df = pd.read_csv(path)
+            phq_column = (
+                "PHQ_Binary" if "PHQ_Binary" in phq_df.columns else "PHQ8_Binary"
+                if "PHQ8_Binary" in phq_df.columns
+                else None
+            )
+            
+            if phq_column:
+                phq_df = phq_df[["Participant_ID", phq_column]]
+                phq_df.rename(columns={phq_column: "PHQ_Binary"}, inplace=True)
+                phq_dataframes.append(phq_df)
+
+        combined_phq_df = pd.concat(phq_dataframes, ignore_index=True)
+        df = df.merge(combined_phq_df, how="left", left_on="ID", right_on="Participant_ID")
+        df.drop(columns=["Participant_ID"], inplace=True)
+        return df.dropna(subset=["PHQ_Binary"])
+
+    def _get_feature_subset_df(self, path_map: pd.DataFrame) -> pd.DataFrame:
+        df = pd.DataFrame()
+
+        for i in path_map.index:
+            df_concat = pd.DataFrame()
+            for column, (preprocess_func, prefix) in self.feature_map.items():
+                if column in path_map.columns:
+                    processed_feature = preprocess_func(path_map[column][i]).add_prefix(
+                        prefix
+                    )
+                    df_concat = pd.concat([df_concat, processed_feature], axis=1)
+            df_concat["ID"] = path_map["ID"].iloc[i]
+            df = pd.concat([df, df_concat], ignore_index=True)
+
+        df.columns = df.columns.str.replace(r"[^\w]", "", regex=True)
+        return df
+
+    def _process_temporal_features(
+        self, df: pd.DataFrame, timestamp_col: str, ds_freq: str, rw_size: str
+    ) -> pd.DataFrame:
+        df["TIMESTAMP"] = df[timestamp_col]
+        df = df.drop(
+            columns=[col for col in df.columns if "timestamp" in col and col != "TIMESTAMP"]
+        )
+        
+        df["TIMESTAMP"] = pd.to_timedelta(df["TIMESTAMP"], unit="s")
+        df.set_index(["ID", "TIMESTAMP"], inplace=True)
+        
+        if "AUDIO" in timestamp_col:
+            df = df.groupby("ID").resample("33.3311ms", level="TIMESTAMP").mean()
+            
+        df = df.reset_index()
+        df["TIMESTAMP"] = df["TIMESTAMP"].apply(lambda x: x.round("10ms"))
+        df.set_index(["ID", "TIMESTAMP"], inplace=True)
+
+        df_resampled = df.groupby("ID").resample(ds_freq, level="TIMESTAMP").mean()
+        return (
+            df_resampled.groupby(level="ID")
+            .rolling(rw_size, on=df_resampled.index.get_level_values("TIMESTAMP"))
+            .mean()
+            .reset_index(level=0, drop=True)
+        )
+
+
+class ResultsLoader(DataLoader):
+    def __init__(self, base_directory: str = "data"):
+        super().__init__(base_directory)
+        self.feature_map = {}
+
+    def get_data(self, percentage: float = 0, random_state: int = 42) -> pd.DataFrame:
+        balanced_subset = self.get_balanced_subset(percentage, random_state)
+        df = self.get_path_map(folder_ids=balanced_subset)
+        df = self._append_PHQ_Binary(df)
+        return df.set_index("ID")
+
+
+class TextLoader(DataLoader):
+    def __init__(self, base_directory: str = "data"):
+        super().__init__(base_directory)
+        self.feature_map = {
+            "TRANSCRIPT": (pp_text.preprocess_TRANSCRIPT, "TRANSCRIPT_"),
+        }
+        self.required_files = ["TRANSCRIPT.csv"]
+
+    def get_data(self, percentage: float = 0, random_state: int = 42) -> pd.DataFrame:
+        balanced_subset = self.get_balanced_subset(percentage, random_state)
+        path_map = self.get_path_map(
+            required_files=self.required_files, 
+            folder_ids=balanced_subset
+        )
+        df = self._get_feature_subset_df(path_map)
+        return df.set_index("ID")
+
+
+class AudioLoader(DataLoader):
+    def __init__(self, base_directory: str = "data"):
+        super().__init__(base_directory)
+        self.feature_map = {
+            "AUDIO": (pp_audio.preprocess_AUDIO, "AUDIO_"),
+            "FORMANT": (pp_audio.preprocess_FORMANT, "FORMANT_"),
+            "COVAREP": (pp_audio.preprocess_COVAREP, "COVAREP_"),
+        }
+        self.required_files = ["AUDIO.wav", "FORMANT.csv", "COVAREP.csv"]
+
+    def get_data(
+        self,
+        percentage: float = 0,
+        random_state: int = 42,
+        ds_freq: str = "50ms",
+        rw_size: str = "10s",
+    ) -> pd.DataFrame:
+        balanced_subset = self.get_balanced_subset(percentage, random_state)
+        path_map = self.get_path_map(
+            required_files=self.required_files, 
+            folder_ids=balanced_subset
+        )
+        df = self._get_feature_subset_df(path_map)
+        return self._process_temporal_features(df, "FORMANT_timestamp", ds_freq, rw_size)
+
+
+class FaceLoader(DataLoader):
+    def __init__(self, base_directory: str = "data"):
+        super().__init__(base_directory)
+        self.feature_map = {
+            "CLNF_gaze": (pp_face.preprocess_CLNF_gaze, "CLNFgaze_"),
+            "CLNF_AUs": (pp_face.preprocess_CLNF_AUs, "CLNFAUs_"),
+            "CLNF_hog": (pp_face.preprocess_CLNF_hog, "CLNFhog_"),
+            "CLNF_features": (pp_face.preprocess_CLNF_features, "CLNFfeatures_"),
+            "CLNF_pose": (pp_face.preprocess_CLNF_pose, "CLNFpose_"),
+            "CLNF_features3D": (pp_face.preprocess_CLNF_features3D, "CLNFfeatures3D_"),
+        }
+        self.required_files = [
+            "CLNF_gaze.txt", "CLNF_AUs.txt", "CLNF_hog.bin",
+            "CLNF_features.txt", "CLNF_pose.txt", "CLNF_features3D.txt"
+        ]
+
+    def get_data(
+        self,
+        percentage: float = 0,
+        random_state: int = 42,
+        ds_freq: str = "50ms",
+        rw_size: str = "10s",
+    ) -> pd.DataFrame:
+        balanced_subset = self.get_balanced_subset(percentage, random_state)
+        path_map = self.get_path_map(
+            required_files=self.required_files, 
+            folder_ids=balanced_subset
+        )
+        df = self._get_feature_subset_df(path_map)
+        df = self._process_temporal_features(df, "CLNFgaze_timestamp", ds_freq, rw_size)
+        return df
+
+    def _clean_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.drop(columns=[
+            col for col in df.columns
+            if any(substring in col for substring in ["frame", "confidence", "success", "is_valid"])
+        ])
---
+++ DepressionDetection/data_load/pp_audio.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+import librosa
+
+
+def add_timestamps(df, sampling_rate=100):
+    # Calculate time interval based on the sampling rate
+    time_interval = 1 / sampling_rate
+
+    # Generate timestamps based on the number of samples
+    num_samples = len(df)
+    timestamps = np.arange(0, num_samples * time_interval, time_interval)
+
+    # Add the timestamps as a new column in the DataFrame
+    df["timestamp"] = timestamps[:num_samples]
+
+    return df
+
+
+def preprocess_AUDIO(path):
+    # TODO: is it necessary to resample the audio data to 100Hz? it might not be usefull at this sampling rate
+    # Load the audio file
+    y, sr = librosa.load(path)
+
+    # Resample to 100Hz to match the 100Hz sampling rate of the COVAREP and FORMANT data
+    target_sr = 100
+    y_resampled = librosa.resample(y=y, orig_sr=sr, target_sr=target_sr)
+
+    # Create timestamps (in seconds) for each sample
+    timestamps = np.arange(len(y_resampled)) / target_sr
+
+    # Create a DataFrame with timestamps and audio samples
+    audio_df = pd.DataFrame({
+        'timestamp': timestamps,
+        'AMPLITUDE': y_resampled
+    })
+
+    return audio_df
+
+
+def preprocess_FORMANT(path):
+    formant_features = pd.read_csv(path, header=None)
+    FORMANT_FEATURES = ["F1", "F2", "F3", "F4", "F5"]
+    formant_features.columns = FORMANT_FEATURES
+    formant_features = add_timestamps(formant_features)
+    return formant_features
+
+
+def preprocess_COVAREP(path):
+    covarep_features = pd.read_csv(path, header=None)
+    COVAREP_FEATURES = [
+        "F0",
+        "VUV",
+        "NAQ",
+        "QOQ",
+        "H1H2",
+        "PSP",
+        "MDQ",
+        "peakSlope",
+        "Rd",
+        "Rd_conf",
+        "creak",
+        "MCEP_0",
+        "MCEP_1",
+        "MCEP_2",
+        "MCEP_3",
+        "MCEP_4",
+        "MCEP_5",
+        "MCEP_6",
+        "MCEP_7",
+        "MCEP_8",
+        "MCEP_9",
+        "MCEP_10",
+        "MCEP_11",
+        "MCEP_12",
+        "MCEP_13",
+        "MCEP_14",
+        "MCEP_15",
+        "MCEP_16",
+        "MCEP_17",
+        "MCEP_18",
+        "MCEP_19",
+        "MCEP_20",
+        "MCEP_21",
+        "MCEP_22",
+        "MCEP_23",
+        "MCEP_24",
+        "HMPDM_0",
+        "HMPDM_1",
+        "HMPDM_2",
+        "HMPDM_3",
+        "HMPDM_4",
+        "HMPDM_5",
+        "HMPDM_6",
+        "HMPDM_7",
+        "HMPDM_8",
+        "HMPDM_9",
+        "HMPDM_10",
+        "HMPDM_11",
+        "HMPDM_12",
+        "HMPDM_13",
+        "HMPDM_14",
+        "HMPDM_15",
+        "HMPDM_16",
+        "HMPDM_17",
+        "HMPDM_18",
+        "HMPDM_19",
+        "HMPDM_20",
+        "HMPDM_21",
+        "HMPDM_22",
+        "HMPDM_23",
+        "HMPDM_24",
+        "HMPDD_0",
+        "HMPDD_1",
+        "HMPDD_2",
+        "HMPDD_3",
+        "HMPDD_4",
+        "HMPDD_5",
+        "HMPDD_6",
+        "HMPDD_7",
+        "HMPDD_8",
+        "HMPDD_9",
+        "HMPDD_10",
+        "HMPDD_11",
+        "HMPDD_12",
+    ]
+    covarep_features.columns = COVAREP_FEATURES
+    covarep_features = add_timestamps(covarep_features)
+    return covarep_features
