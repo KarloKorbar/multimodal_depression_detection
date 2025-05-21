75f77400d0de11c805726d818e18378e62e47d2d
karlo <karlo.korbi@gmail.com>
2025-05-21 16:54:37 +0200

    moved text preprocessing from text.ipynb to loader_text.py

--- DepressionDetection/preprocessing/loader_text.py
+++ DepressionDetection/preprocessing/loader_text.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Download required NLTK data
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("punkt")
 
 from preprocessing.loader import DataLoader
 from preprocessing.pp import pp_text as pp_text
@@ -12,10 +20,18 @@ class TextLoader(DataLoader):
         }
         self.required_files = ["TRANSCRIPT.csv"]
 
+    def _preprocess_text(self, text: str) -> str:
+        stop_words = set(stopwords.words("english"))
+        lemmatizer = WordNetLemmatizer()
+        words = text.split()
+        words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+        return " ".join(words)
+
     def get_data(self, percentage: float = 0, random_state: int = 42) -> pd.DataFrame:
         balanced_subset = self.get_balanced_subset(percentage, random_state)
         path_map = self.get_path_map(
             required_files=self.required_files, folder_ids=balanced_subset
         )
         df = self._get_feature_subset_df(path_map)
+        df["TRANSCRIPT_text"] = df["TRANSCRIPT_text"].apply(self._preprocess_text)
         return df.set_index("ID")
